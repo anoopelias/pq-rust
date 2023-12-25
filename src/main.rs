62dac4ec7c6f58bfe603bb8c4f0286d1ada362e8
Anoop Elias <anoopelias@gmail.com>
2023-12-25 21:11:16 +0530

    Implement pq thru inefficient sorting

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ impl<T: Ord> Pq<T> {
 
     pub fn insert(&mut self, value: T) {
         self.values.push(value);
+        self.values.sort_by(|a, b| b.cmp(a));
     }
 
     pub fn remove(&mut self) -> Option<T> {
@@ -27,9 +28,25 @@ mod tests {
     use super::Pq;
 
     #[test]
-    fn insert() {
+    fn insert_remove() {
         let mut pq = Pq::new();
         pq.insert(10);
+        assert_eq!(Some(10), pq.remove());
+    }
+
+    #[test]
+    fn insert_remove_in_order() {
+        let mut pq = Pq::new();
+        pq.insert(12);
+        pq.insert(10);
+        assert_eq!(Some(10), pq.remove());
+    }
+
+    #[test]
+    fn insert_remove_out_of_order() {
+        let mut pq = Pq::new();
+        pq.insert(10);
+        pq.insert(12);
         assert_eq!(Some(10), pq.remove());
     }
 }
